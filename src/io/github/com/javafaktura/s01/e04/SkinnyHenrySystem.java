48d7ae8e91331d582074af897a49876555406217
Tobiasz Kowalski <tob.kowalski@gmail.com>
2019-05-05 11:51:04 +0200

    ADD_PIZZA builder

--- src/io/github/com/javafaktura/s01/e04/SkinnyHenrySystem.java
+++ src/io/github/com/javafaktura/s01/e04/SkinnyHenrySystem.java
@@ -1,7 +1,5 @@
 package io.github.com.javafaktura.s01.e04;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 import io.github.com.javafaktura.s01.e04.model.*;
@@ -20,62 +18,33 @@ public class SkinnyHenrySystem {
 
     private void createPizza() {
         try(Scanner in = new Scanner(System.in)) {
+            PizzaBuilder pizzaBuilder = new PizzaBuilder();
+
             System.out.println("Give me name for your pizza");
-            String pizzaName = getName(in.nextLine());
+            pizzaBuilder.withName(in.nextLine());
 
             System.out.println("Which dough? (ITALIAN, POLISH, PLUMB)");
-            Dough dough = getDough(in.nextLine());
+            pizzaBuilder.withDough(in.nextLine());
 
             System.out.println("Which sauce? (TOMATO, CHEESE, TOMATO_CHEESE)");
-            Sauce sauce = getSauce(in.nextLine());
+            pizzaBuilder.withSauce(in.nextLine());
 
             System.out.println("Which size? (SMALL, MEDIUM, LARGE, EXTRA_LARGE)");
-            Size size = getSize(in.nextLine());
+            pizzaBuilder.withSize(in.nextLine());
 
-            List<Topping> toppings = new ArrayList<>();
             while (true) {
                 System.out.println("Which topping do you want?");
-                toppings.add(Topping.valueOf(in.nextLine()));
+                pizzaBuilder.withTopping(Topping.valueOf(in.nextLine()));
 
                 System.out.println("More toppings? (YES/NO)");
-                String nextTopping = in.nextLine();
 
-                if (nextTopping.equals("NO")) {
+                if (in.nextLine().equals("NO")) {
                     break;
                 }
             }
 
-            Pizza pizza = new Pizza(pizzaName, dough, sauce, size, toppings);
+            Pizza pizza = pizzaBuilder.build();
             System.out.println(pizza);
         }
-    }
-
-    private String getName(String pizzaName) {
-        if (pizzaName.isBlank()) {
-            pizzaName = "Mine pizza";
-        }
-        return pizzaName;
-    }
-
-    private Size getSize(String sizeValue) {
-        if (sizeValue.isBlank()) {
-            sizeValue = "MEDIUM";
-        }
-        return Size.valueOf(sizeValue);
-    }
-
-    private Sauce getSauce(String sauceValue) {
-        if (sauceValue.isBlank()) {
-            sauceValue = "TOMATO_CHEESE";
-        }
-        return Sauce.valueOf(sauceValue);
-    }
-
-    private Dough getDough(String doughValue) {
-        if (doughValue.isBlank()) {
-            doughValue = "ITALIAN";
-        }
-
-        return Dough.valueOf(doughValue);
     }
 }
